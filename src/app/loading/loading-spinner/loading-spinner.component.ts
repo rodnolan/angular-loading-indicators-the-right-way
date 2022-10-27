cbcfd325f63fd7ec08696fdd2e1dd1be965c9420
Rod Nolan <rnolan@aquent.com>
2022-10-27 13:19:16 -0400

    reorg styles and format html

--- src/app/loading/loading-spinner/loading-spinner.component.ts
+++ src/app/loading/loading-spinner/loading-spinner.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-loading-spinner",
-  templateUrl: "./loading-spinner.component.html"
+  templateUrl: "./loading-spinner.component.html",
+  styleUrls: ["./loading-spinner.css"]
 })
 export class LoadingSpinnerComponent {}
